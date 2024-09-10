40c6eff913a2d4f3ee3fb37bf9cc465a352d2096
Johni212 <140275117+Johni212@users.noreply.github.com>
2024-09-10 22:58:00 +0100

    Update Data.py

--- Data.py
+++ Data.py
@@ -1,3 +1,11 @@
 pip install matplotlib 
 pip install numpy
 pip install pandas
+pip install seaborn
+pip install librosa
+
+import numpy as np
+import pandas as pd
+import matplotlib.pylab as plt
+import seaborn as sns
+import librosa
